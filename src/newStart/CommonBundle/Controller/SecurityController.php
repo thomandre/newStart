330a84debc7ebed29a6ab27a0a30550834b62195
vagrant <vagrant@precise64.(none)>
2013-09-28 12:51:33 +0000

    wip

---
+++ src/newStart/CommonBundle/Controller/SecurityController.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace newStart\CommonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use newStart\CommonBundle\Entity\BetaUser;
+use JMS\DiExtraBundle\Annotation as DI;
+
+
+class SecurityController extends Controller {
+	
+	    /**
+     * @DI\Inject("doctrine.orm.entity_manager")
+     * @var \Doctrine\ORM\EntityManager
+     */
+    public $em;
+
+    /**
+     * @Route("/fb_login_check")
+     * @Template()
+     */
+    public function loginAction(Request $request)
+    {
+    	var_dump('plop');
+		return array();
+	}		
+
+    /**
+     * @Route("/logout")
+     * @Template()
+     */
+    public function logoutAction(Request $request)
+    {
+		return array();
+	}		
+
+}
