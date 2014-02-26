0818771a9243dc27b8f97acfdf731e81793e7b0c
thomandre <thomandre@gmail.com>
2014-02-26 11:46:00 +0100

    adding fixtures load to phpunit.sh

--- phpunit.sh
+++ phpunit.sh
@@ -2,5 +2,6 @@
 ./sf doctrine:database:drop --force --env=test
 ./sf doctrine:database:create --env=test
 ./sf doctrine:schema:update --force --env=test
+./sf doctrine:fixture:load --env=test -n
 
 phpunit --strict -c app $1 $2
