8a9203147f46bcbd3d2ec0a5ec5e0a94f7b938f3
thomandre <thomandre@gmail.com>
2014-05-05 17:59:09 +0200

    debug toolbar en beta

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,7 +28,7 @@ class AppKernel extends Kernel
             new Minifier\MinifierBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'beta'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
